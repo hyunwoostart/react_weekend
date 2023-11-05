b30653cc053992322bcf4999dc4151c9d4684a98
hyunoostart <knjhw@daum.net>
2023-11-05 13:53:09 +0900

    리액트수업 업로드

--- src/components/main/news/News.jsx
+++ src/components/main/news/News.jsx
@@ -1,14 +1,25 @@
 import './News.scss';
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 
 function News() {
+	const dummyData = useRef([
+		{ title: 'title1', content: 'content1', date: new Date() },
+		{ title: 'title2', content: 'content2', date: new Date() },
+		{ title: 'title3', content: 'content3', date: new Date() },
+		{ title: 'title4', content: 'content4', date: new Date() },
+		{ title: 'title5', content: 'content5', date: new Date() },
+	]);
 	const getLocalData = () => {
 		const data = localStorage.getItem('posts');
 		if (data) return JSON.parse(data);
-		else return [];
+		else return dummyData.current;
 	};
 
 	const [News] = useState(getLocalData());
+
+	useEffect(() => {
+		localStorage.setItem('posts', JSON.stringify(News));
+	}, [News]);
 
 	return (
 		<section className='news'>
