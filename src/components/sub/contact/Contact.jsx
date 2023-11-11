cb82bf15ce64f2f43982488879a1a02365116e84
hyunwoostart <knjhw@daum.net>
2023-11-11 12:58:44 +0900

    지도연결 세팅

--- src/components/sub/contact/Contact.jsx
+++ src/components/sub/contact/Contact.jsx
@@ -1,9 +1,10 @@
 import Layout from '../../common/layout/Layout';
+import './Contact.scss';
 
 export default function Contact() {
 	return (
 		<Layout title={'Contact us'}>
-			<p>찾아오는길 상세페이지</p>
+			<article id='map'></article>
 		</Layout>
 	);
 }
