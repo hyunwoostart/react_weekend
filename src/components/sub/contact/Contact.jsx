6b89d0ccdf5e1002568086217e94724419f67bc6
hyunwoostart <knjhw@daum.net>
2023-11-11 13:45:19 +0900

    ddd

--- src/components/sub/contact/Contact.jsx
+++ src/components/sub/contact/Contact.jsx
@@ -1,10 +1,27 @@
+import { useRef, useEffect } from 'react';
 import Layout from '../../common/layout/Layout';
 import './Contact.scss';
 
 export default function Contact() {
+	const { kakao } = window;
+	//api 적용할 요소도 가상돔이기 때문에 참조객체에 연결
+	const mapFrame = useRef(null);
+	const mapOption = {
+		center: new kakao.maps.LatLng(37.510635, 127.058025),
+		level: 3,
+	};
+	const marker = new kakao.maps.Marker({
+		position: mapOption.center,
+	});
+
+	useEffect(() => {
+		const map = new kakao.maps.Map(mapFrame.current, mapOption);
+		marker.setMap(map);
+	}, []);
+
 	return (
 		<Layout title={'Contact us'}>
-			<article id='map'></article>
+			<article id='map' ref={mapFrame}></article>
 		</Layout>
 	);
 }
