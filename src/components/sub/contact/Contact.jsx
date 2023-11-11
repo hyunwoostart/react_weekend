3eebcf1c5756799afc11855ff1b71c53992c7c16
hyunwoostart <knjhw@daum.net>
2023-11-11 14:01:05 +0900

    마커이미지 변경

--- src/components/sub/contact/Contact.jsx
+++ src/components/sub/contact/Contact.jsx
@@ -10,14 +10,22 @@ export default function Contact() {
 		center: new kakao.maps.LatLng(37.51177006335203, 127.05745288988979),
 		level: 3,
 	};
-	const marker = new kakao.maps.Marker({
-		position: mapOption.center,
-	});
 
 	useEffect(() => {
 		const map = new kakao.maps.Map(mapFrame.current, mapOption);
 		marker.setMap(map);
 	}, []);
+
+	// 마커이미지 인스턴스를 생성하기 위한 정보값들
+	const imageSrc = 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_red.png';
+	const imageSize = new kakao.maps.Size(64, 69);
+	const imageOption = { offset: new kakao.maps.Point(27, 69) };
+	const markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
+
+	const marker = new kakao.maps.Marker({
+		position: mapOption.center,
+		image: markerImage,
+	});
 
 	return (
 		<Layout title={'Contact us'}>
