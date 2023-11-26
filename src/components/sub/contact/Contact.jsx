ae4b2f4725c6c48e1c10db8e83328b3c7421720e
hwji <knjhw@daum.net>
2023-11-26 13:07:57 +0900

    useMemo와 useCallback을 통한 메모이제이션

--- src/components/sub/contact/Contact.jsx
+++ src/components/sub/contact/Contact.jsx
@@ -1,4 +1,4 @@
-import { useRef, useEffect, useState } from 'react';
+import { useRef, useEffect, useState, useCallback, useMemo } from 'react';
 import Layout from '../../common/layout/Layout';
 import './Contact.scss';
 import emailjs from '@emailjs/browser';
@@ -38,14 +38,18 @@ export default function Contact() {
 		},
 	]);
 
-	const marker = new kakao.maps.Marker({
-		position: info.current[Index].latlng,
-		image: new kakao.maps.MarkerImage(
-			info.current[Index].imgSrc,
-			info.current[Index].imgSize,
-			info.current[Index].imgPos
-		),
-	});
+	// useMemo는 함수자체가 아닌 함수가 반환하는 리턴값을 메모이제이션처리하기 때문에
+	// 무조건 첫번째 인수값으로 특정값을 리턴하는 익명함수가 등록되어야함
+	const marker = useMemo(() => {
+		return new kakao.maps.Marker({
+			position: info.current[Index].latlng,
+			image: new kakao.maps.MarkerImage(
+				info.current[Index].imgSrc,
+				info.current[Index].imgSize,
+				info.current[Index].imgPos
+			),
+		});
+	}, [kakao, Index]);
 
 	const sendEmail = (e) => {
 		e.preventDefault();
@@ -75,18 +79,18 @@ export default function Contact() {
 		);
 	};
 
-	const roadView = () => {
+	const roadView = useCallback(() => {
 		//roadview setting
 		//두번째 인수값 50(m단위)은 마커위치로부터 로드뷰가 출력될수 있는 가장 가까운 거리의 범위지정
 		new kakao.maps.RoadviewClient().getNearestPanoId(info.current[Index].latlng, 50, (id) => {
 			new kakao.maps.Roadview(viewFrame.current).setPanoId(id, info.current[Index].latlng);
 		});
-	};
+	}, [Index, kakao]);
 
-	const setCenter = () => {
+	const setCenter = useCallback(() => {
 		mapInstance.current.setCenter(info.current[Index].latlng);
 		roadView();
-	};
+	}, [Index, roadView]);
 
 	useEffect(() => {
 		mapFrame.current.innerHTML = '';
@@ -106,18 +110,18 @@ export default function Contact() {
 		roadView();
 
 		window.addEventListener('resize', setCenter);
-	}, [Index]);
+	}, [Index, kakao, marker, roadView, setCenter]);
 
 	//교통정보 보기 토글 기능
 	useEffect(() => {
 		Traffic
 			? mapInstance.current.addOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC)
 			: mapInstance.current.removeOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
-	}, [Traffic]);
+	}, [Traffic, kakao]);
 
 	useEffect(() => {
 		return () => window.removeEventListener('resize', setCenter);
-	}, []);
+	}, [setCenter]);
 
 	return (
 		<Layout title={'Contact us'}>
