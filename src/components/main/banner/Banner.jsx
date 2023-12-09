560dfa713d10ac4deff926bceee2a39a9b46af66
hwji <knjhw@daum.net>
2023-12-09 13:47:35 +0900

    코드 수정

--- src/components/main/banner/Banner.jsx
+++ src/components/main/banner/Banner.jsx
@@ -5,21 +5,21 @@ import { useRef, useEffect, useCallback } from 'react';
 export default function Banner() {
 	const currentEl = useRef(null);
 	const titleEl = useRef(null);
-	const getScroll = useGetCurrentScroll();
+	const scrollFrame = currentEl.current?.parentElement.parentElement;
+	const getScroll = useGetCurrentScroll(scrollFrame);
 
-	// handleScroll에 대입되어 있는 함수가 컴포넌트 재랜더링시마다 계속 읽히기 때문에
-	// useCallback을 통해서 강제로 메모리에 등록해서 기존함수내용을 제활용하는 메모이제이션 처리
-	// 메모이제이션되는 순간 그안쪽의 모든 값들이 static하게 고정되어 버리므로
-	// 의존성배열에 특정 값을 지정해서 해당 값이 변경시에는 임시로 메모이제이션 해제
+	//hadleScroll에 대입되어 있는 함수가 컴포넌트 재랜더링시마다 계속 읽히기 때문에
+	//useCallback을 통해서 강제로 메모리에 등록해서 기존함수내용을 재활용하는 메모이제이션 처리
+	//메모이제이션되는 순간 그안쪽의 모든 값들이 static하게 고정되어 버리므로
+	//의존성배열에 특정 값을 지정해서 해당 값이 변경시에는 임시로 메모이제이션 해제
 	const handleScroll = useCallback(() => {
 		const modifiedScroll = getScroll(currentEl);
 		titleEl.current.style.transform = `translateX(${modifiedScroll}px)`;
 	}, [getScroll]);
 
 	useEffect(() => {
-		window.addEventListener('scroll', handleScroll);
-		return () => window.removeEventListener('scroll', handleScroll);
-	}, [handleScroll]);
+		scrollFrame?.addEventListener('scroll', handleScroll);
+	}, [handleScroll, scrollFrame]);
 
 	return (
 		<section className='banner myScroll' ref={currentEl}>
