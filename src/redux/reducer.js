adf90c197b7c2f7a8dc9297b5da5a0c9c6eec0f6
hwji <knjhw@daum.net>
2023-12-16 14:20:11 +0900

    비동기 데이터 전역 스토어에 저장되고 활용되는 흐름 정리

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -9,5 +9,6 @@ const memberReducer = (state = { members: [] }, action) => {
 	}
 };
 
+//순서1 - 리듀서함수가 실행되는 시점에는 전역에 담을 비동기 데이터가 없으므로 빈배열로 전역state 초기화
 const reducers = combineReducers({ memberReducer });
 export default reducers;
