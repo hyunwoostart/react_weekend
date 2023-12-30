b038966ab8bec8aa6d78fbb38a30c060db030f05
hwji <knjhw@daum.net>
2023-12-30 14:58:15 +0900

    history, department데이터 react-query로 관리

--- src/components/sub/department/Department.jsx
+++ src/components/sub/department/Department.jsx
@@ -1,72 +1,62 @@
 import './Department.scss';
 import Layout from '../../common/layout/Layout';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useMembersQuery } from '../../../hooks/useMembersQuery';
+import { useHistoryQuery } from '../../../hooks/useHistoryQuery';
 
 const path = process.env.PUBLIC_URL;
 
 export default function Department() {
-	const [Num, setNum] = useState(0);
-	const [History, setHistory] = useState([]);
-
-	//커스텀훅 호출시 인수로 데이터에서 뽑아낼 데이터의 순번을 전달
-	const { data } = useMembersQuery(Num);
-	console.log(data);
-
-	const fetchHistory = async () => {
-		const data = await fetch(`${path}/DB/history.json`);
-		const json = await data.json();
-
-		setHistory(json.history);
-	};
-
-	useEffect(() => {
-		fetchHistory();
-	}, []);
+	const {
+		isLoading: memberLoading,
+		isError: memberErr,
+		isSuccess: memberSuccess,
+		data: member,
+	} = useMembersQuery();
+	const { isSuccess: historySuccess, data: history } = useHistoryQuery();
 
 	return (
 		<Layout title={'Department'}>
-			<button onClick={() => setNum(0)}>데이터0 확인</button>
-			<button onClick={() => setNum(1)}>데이터1 확인</button>
 			<section id='historyBox'>
 				<h2>History</h2>
 				<div className='con'>
-					{History.map((data, idx) => {
-						return (
-							<React.Fragment key={idx}>
-								<h3>{Object.keys(data)[0]}</h3>
-								<ul>
-									{Object.values(data)[0].map((val, idx) => (
-										<li key={idx}>{val}</li>
-									))}
-								</ul>
-							</React.Fragment>
-						);
-					})}
+					{historySuccess &&
+						history.map((data, idx) => {
+							return (
+								<React.Fragment key={idx}>
+									<h3>{Object.keys(data)[0]}</h3>
+									<ul>
+										{Object.values(data)[0].map((val, idx) => (
+											<li key={idx}>{val}</li>
+										))}
+									</ul>
+								</React.Fragment>
+							);
+						})}
 				</div>
 			</section>
 
-			{/* <section id="memberBox">
-        <h2>Department</h2>
+			<section id='memberBox'>
+				<h2>Department</h2>
 
-        <div className="con">
-          {isLoading && <p>Loading...</p>}
-          {isSuccess &&
-            Department.map((member, idx) => {
-              return (
-                <article key={idx}>
-                  <div className="pic">
-                    <img src={`${path}/img/${member.pic}`} alt={member.name} />
-                  </div>
-                  <h3>{member.name}</h3>
-                  <p>{member.position}</p>
-                </article>
-              );
-            })}
+				<div className='con'>
+					{memberLoading && <p>Loading...</p>}
+					{memberSuccess &&
+						member.map((member, idx) => {
+							return (
+								<article key={idx}>
+									<div className='pic'>
+										<img src={`${path}/img/${member.pic}`} alt={member.name} />
+									</div>
+									<h3>{member.name}</h3>
+									<p>{member.position}</p>
+								</article>
+							);
+						})}
 
-          {isError && <p>Fail to load Data</p>}
-        </div>
-      </section> */}
+					{memberErr && <p>Fail to load Data</p>}
+				</div>
+			</section>
 		</Layout>
 	);
 }
